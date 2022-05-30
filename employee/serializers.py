7f2a49d0e04f454c449d97b846bd89085f7b494d
Gokhan OKUMUS <okumusg@windowslive.com>
2022-05-30 14:42:02 +0200

    Added doc string for serializer classes

--- employee/serializers.py
+++ employee/serializers.py
@@ -3,41 +3,65 @@ from .models import Employee, Team, TeamEmployee, TeamLeader, WorkArrangement, C
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Employee model
+    """
     total_monthly_payment = serializers.SerializerMethodField()
 
     def get_total_monthly_payment(self, employee_obj):
+        """Return total monthly payment of an employee"""
         return employee_obj.get_total_monthly_payment()
 
     class Meta:
         model = Employee
-        fields = "__all__"
+        fields = ['id', 'employee_id', 'name', 'hourly_rate', 'date_joined', 'total_monthly_payment']
 
 
 class TeamSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Team model
+    """
+
     class Meta:
         model = Team
-        fields = "__all__"
+        fields = ['id', 'name', 'is_active', 'creation_time']
 
 
 class TeamEmployeeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for TeamEmployee model
+    """
+
     class Meta:
         model = TeamEmployee
         fields = "__all__"
 
 
 class TeamLeaderSerializer(serializers.ModelSerializer):
+    """
+    Serializer for TeamLeader model
+    """
+
     class Meta:
         model = TeamLeader
         fields = "__all__"
 
 
 class WorkArrangementSerializer(serializers.ModelSerializer):
+    """
+    Serializer for WorkArrangement model
+    """
+
     class Meta:
         model = WorkArrangement
         fields = "__all__"
 
 
 class ContractSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Contract model
+    """
+
     class Meta:
         model = Contract
         fields = "__all__"
